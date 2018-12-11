8b6ced5ac32d0d2ae6d52476d569acc3d4971978
rainvagel <rain.vagel@gmail.com>
2018-12-11 09:22:26 +0200

    Functions to read from file to pandas dataframe and split into dataframes by SubjectID added.

--- src/reader.py
+++ src/reader.py
@@ -1,1 +1,15 @@
 # Here we will have the functions that will read in the data and also manipulate it
+
+import pandas as pd
+
+
+def reader(filename):
+    data = pd.read_csv("../data/"+filename)
+    return data
+
+
+def student_splitter(dataframe):
+    ID = {}
+    for id in dataframe.SubjectID.unique():
+        ID[id] = dataframe.loc[dataframe['SubjectID'] == id]
+    return ID
